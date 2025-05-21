3fbec5ff5a5b29095dc9263dd2cb9f99099677bb
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-05-21 22:01:52 +0300

    fix: reorganize folders, add icons and images for home tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,28 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
 
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <ImageBackground source={images.highlight}>
+              <Image
+                source={icons.home}
+                tintColor="#151312"
+                className="size-5"
+              />
+              <Text>Home</Text>
+            </ImageBackground>
+          ),
+        }}
       />
       <Tabs.Screen
         name="search"
