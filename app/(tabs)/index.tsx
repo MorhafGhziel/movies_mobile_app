6d0bf992fb490cd4365425e2fb9681d9e7520097
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-05-25 16:12:14 +0300

    style: enhance ScrollView with contentContainerStyle for better layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,8 +7,13 @@ export default function Index() {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="w-full absolute z-0 " />
 
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-row items-center justify-center"></View>
       </ScrollView>
     </View>
   );
