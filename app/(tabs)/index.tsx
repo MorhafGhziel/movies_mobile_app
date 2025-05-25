eb029a51b78846749057cff94e748a5185e59bc2
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-05-25 16:09:17 +0300

    style: disable vertical scroll indicator for cleaner UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="w-full absolute z-0 " />
 
-      <ScrollView className="flex-1 px-5">
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
       </ScrollView>
     </View>
